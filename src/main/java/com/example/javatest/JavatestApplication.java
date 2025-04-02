2a5d4edaa12013ed63962add38660662323d8bda
denkatastoynev <denkatastoynev@gmail.com>
2025-04-02 15:32:23 +0300

    first commit

---
+++ src/main/java/com/example/javatest/JavatestApplication.java
@@ -0,0 +1,14 @@
+package com.example.javatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+		SpringApplication.run(JavatestApplication.class, args);
+	}
+
+}
